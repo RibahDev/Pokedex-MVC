50732782c309138552e11be79d56894a13a79f62
Ribeiro <joao.ribeiro17@outlook.com>
2024-02-19 20:38:23 -0300

    .

--- Pokedex/Models/Pokemon.cs
+++ Pokedex/Models/Pokemon.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Pokedex.Models
-{
+namespace Pokedex.Models;
     public class Pokemon
     {
         
     }
-}
