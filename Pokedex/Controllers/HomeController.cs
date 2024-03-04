3d02f9244b5949acfbe7f1afff2ca69e01f9aafc
Ribeiro <joao.ribeiro17@outlook.com>
2024-03-04 20:21:11 -0300

    Ajustando e Finalizando Pokedex

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -35,7 +35,30 @@ public class HomeController : Controller
 
     public IActionResult Details(int id)
     {
-        return View();
+        List<Pokemon> pokemons = [];
+        using (StreamReader leitor = new("Data\\pokemons.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        }
+        List<Tipo> tipos = [];
+        using (StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+        DetailsVM details = new() {
+            Tipos = tipos,
+            Atual = pokemons.FirstOrDefault(p => p.Numero == id),
+            Anterior = pokemons.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero < id),
+            Proximo = pokemons.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero > id),
+        };
+
+        ViewData["Tipos"] = tipos;
+        var pokemon = pokemons
+            .Where(p => p.Numero == id)
+            .FirstOrDefault();
+        return View(pokemon);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
